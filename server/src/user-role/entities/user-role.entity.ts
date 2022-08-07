1d3d012e049bd006840cb784a72c108dd02c6f37
Dave Cranwell <xcession@xcessionsites.com>
2022-08-07 16:51:02 +0100

    Excludes unnecessary fields

--- server/src/user-role/entities/user-role.entity.ts
+++ server/src/user-role/entities/user-role.entity.ts
@@ -1,4 +1,5 @@
 import { UserRoleType } from '@prisma/client'
+import { Exclude } from 'class-transformer'
 
 import { Organisation } from 'src/organisation/entities/organisation.entity'
 import { UserRolesOfUser } from 'src/user-roles-of-user/entities/user-roles-of-user.entity'
@@ -9,7 +10,12 @@ export class UserRole {
   type: UserRoleType
   organisation?: Organisation
   organisationId: number
+
+  @Exclude()
   createdAt: Date
+
+  @Exclude()
   updatedAt: Date
+
   users?: UserRolesOfUser[]
 }
