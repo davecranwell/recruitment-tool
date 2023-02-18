0f629f87e02210335d0b5fcb4e4ca5efccef7977
Dave Cranwell <xcession@xcessionsites.com>
2023-02-18 18:37:28 +0000

    Found a way to debug queries if needed in future

--- apps/server/src/prisma/prisma.service.ts
+++ apps/server/src/prisma/prisma.service.ts
@@ -6,7 +6,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
   private readonly logger = new Logger(PrismaService.name)
 
   constructor() {
-    super({ log: ['info'] })
+    super({ log: ['info'] }) // 'query'
   }
 
   async onModuleInit() {
