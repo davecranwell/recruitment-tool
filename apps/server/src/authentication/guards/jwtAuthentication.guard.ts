69e9146a89b3d51becbc4f61cb74d34f8a06b0cf
Dave Cranwell <xcession@xcessionsites.com>
2023-03-12 17:47:33 +0000

    Hides bad requests

--- apps/server/src/authentication/guards/jwtAuthentication.guard.ts
+++ apps/server/src/authentication/guards/jwtAuthentication.guard.ts
@@ -1,5 +1,13 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
 @Injectable()
-export default class JwtAuthenticationGuard extends AuthGuard('jwt') {}
+export default class JwtAuthenticationGuard extends AuthGuard('jwt') {
+  // Override standard handleRequest to prevent it throwing an annoying error to the log each time a JWT is merely expired
+  handleRequest(err, user, info, context, status) {
+    if (err || !user) {
+      throw new BadRequestException()
+    }
+    return super.handleRequest(err, user, info, context, status)
+  }
+}
