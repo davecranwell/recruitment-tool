07854f1178d07e63b8d7934cfe3cbced9b40e365
Dave Cranwell <xcession@xcessionsites.com>
2023-03-12 21:27:17 +0000

    Adds slightly better error message

--- apps/server/src/authentication/guards/jwtAuthentication.guard.ts
+++ apps/server/src/authentication/guards/jwtAuthentication.guard.ts
@@ -6,7 +6,7 @@ export default class JwtAuthenticationGuard extends AuthGuard('jwt') {
   // Override standard handleRequest to prevent it throwing an annoying error to the log each time a JWT is merely expired
   handleRequest(err, user, info, context, status) {
     if (err || !user) {
-      throw new BadRequestException()
+      throw new BadRequestException('JWT error')
     }
     return super.handleRequest(err, user, info, context, status)
   }
